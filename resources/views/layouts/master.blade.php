8bccd8c279eebd6e158cb6819e0cdc0e233ce715
Ngash <ngashbiz@gmail.com>
2018-03-29 17:03:17 +0300

    added font awesome

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>LipaRent</title>
   <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
