f478962d6849a264a94e21994d985b1f655c9671
Ngash <ngashbiz@gmail.com>
2018-03-29 15:12:48 +0300

    Added admin-lte dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('components');
+});
+
 Auth::routes();
 
 Route::get('/home','PropertyController@index')->name('home');
